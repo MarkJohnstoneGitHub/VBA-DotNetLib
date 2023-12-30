a6599f1d07383ab47ed09ed737bfece60d8b5166
Mark Johnstone <MarkJohnstoneGitHub@users.noreply.github.com>
2023-12-30 20:34:58 +1100

    Updated DotNetLib.tlb
    Added TypeCodeHelperSingleton.cs
    
    https://learn.microsoft.com/en-us/dotnet/api/system.typecode?view=netframework-4.8.1

---
+++ COMDotNetLib/System/ITypeCodeHelperSingleton.cs
@@ -0,0 +1,22 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/api/system.typecode?view=netframework-4.8.1
+
+using GSystem = global::System;
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace DotNetLib.System
+{
+    [ComVisible(true)]
+    [Guid("9A756BE4-62FE-458B-ABCD-A35FA9ABE99B")]
+    [Description("")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface ITypeCodeHelperSingleton
+    {
+        [Description("Converts the value of this instance to its equivalent string representation.")]
+        string ToString(GSystem.TypeCode typecode);
+
+        [Description("Converts the value of this instance to its equivalent string representation using the specified format.")]
+        string ToString2(GSystem.TypeCode typecode, string format);
+    }
+}
---
+++ COMDotNetLib/System/TypeCodeHelperSingleton.cs
@@ -0,0 +1,31 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/api/system.typecode?view=netframework-4.8.1
+
+using GSystem = global::System;
+using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace DotNetLib.System
+{
+    [ComVisible(true)]
+    [Description("Type Code helper to convert a value of this instance to its equivalent string representation.")]
+    [Guid("C28B90C6-FBDA-4942-94F7-CCEED784B9F5")]
+    [ProgId("DotNetLib.System.TypeCodeHelperSingleton")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComDefaultInterface(typeof(ITypeCodeHelperSingleton))]
+    public class TypeCodeHelperSingleton : ITypeCodeHelperSingleton
+    {
+        public TypeCodeHelperSingleton() { }
+
+        public string ToString(GSystem.TypeCode typecode)
+        {
+            return typecode.ToString();
+        }
+
+        public string ToString2(GSystem.TypeCode typecode, string format)
+        {
+            return typecode.ToString(format);
+        }
+
+    }
+}
